b1431e1d8f96eb1e30e6e1a120f4b122a26bae27
IbragimKhaniev <newinterny@gmail.com>
2025-11-26 23:15:22 +0300

    Add Member, Post, and Comment models with admin registration

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Member, Post, Comment
 
-# Register your models here.
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'email', 'created_at']
+    search_fields = ['username', 'email']
+    readonly_fields = ['created_at']
+    list_filter = ['created_at']
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'content_preview', 'created_at', 'updated_at']
+    search_fields = ['content', 'author__username']
+    readonly_fields = ['created_at', 'updated_at']
+    list_filter = ['created_at', 'updated_at']
+    raw_id_fields = ['author']
+
+    def content_preview(self, obj):
+        return obj.content[:50] + '...' if len(obj.content) > 50 else obj.content
+    content_preview.short_description = 'Content'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'post', 'content_preview', 'created_at']
+    search_fields = ['content', 'author__username']
+    readonly_fields = ['created_at']
+    list_filter = ['created_at']
+    raw_id_fields = ['author', 'post']
+
+    def content_preview(self, obj):
+        return obj.content[:50] + '...' if len(obj.content) > 50 else obj.content
+    content_preview.short_description = 'Content'
--- api/models.py
+++ api/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
+from django.utils import timezone
 
-# Create your models here.
+
+class Member(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    bio = models.TextField(blank=True, default='')
+    avatar_url = models.URLField(blank=True, default='')
+    created_at = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        db_table = 'members'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.username
+
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Member, on_delete=models.CASCADE, related_name='posts')
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'posts'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'Post by {self.author.username} at {self.created_at}'
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(Member, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        db_table = 'comments'
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on post {self.post.id}'
